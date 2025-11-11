5f68398bfbef7c7005dc2607d458ee6fcdfed0eb
Sallys Carlos <sallys.carlos93@gmail.com>
2025-11-11 16:58:08 -0300

    feat: index pages

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="loading-screen d-flex align-center justify-center">
+    <v-progress-circular indeterminate color="primary" size="64" />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  .loading-screen {
+    min-height: 100vh;
+    background-color: #0d0d0d;
+  }
+</style>
